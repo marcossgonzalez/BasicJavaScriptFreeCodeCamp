2240610038ccf537bd99ddfef25b74b2244cbdde
Marcos Silveira Gonzalez Júnior <marcos.s.gonzalezjr@outlook.com>
2023-09-22 10:56:38 -0300

    Update lesson
    update lesson

---
+++ numbers.js
@@ -0,0 +1,35 @@
+//Number é um tipo de dado em JavaScript que representa um tipo de dado numérico
+//Lição freecodecamp:
+const sum = 10 + 10;
+//---------------------------
+//Também podemos subtrair um número de outro.
+//JavaScript usa o símbolo - para subtração.
+//Lição freecodecamp:
+const difference = 45 - 33;
+//---------------------------
+/*Também podemos multiplicar um número por outro.
+
+JavaScript usa o símbolo * para multiplicação de dois números.*/
+//Lição freecodecamp:
+const product = 8 * 10;
+//---------------------------
+/*Também podemos dividir um número por outro.
+
+JavaScript usa o símbolo / para divisão.*/
+//Lição freecodecamp:
+const quotient = 66 / 33;
+//---------------------------
+/*Você pode facilmente incrementar ou adicionar um valor a uma variável com o operador ++.*/
+//Lição freecodecamp:
+let myVar = 87;
+
+// Only change code below this line
+myVar ++;
+//---------------------------
+/*Você pode facilmente diminuir ou diminuir uma variável em um com o operador --.*/
+//Lição freecodecamp:
+let myVar2 = 11;
+
+// Only change code below this line
+myVar2--;
+//---------------------------
