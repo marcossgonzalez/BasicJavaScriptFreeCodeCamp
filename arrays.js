713b098dcf3fabc5e66f4b34afe6ecb480449883
Marcos Silveira Gonzalez Júnior <marcos.s.gonzalezjr@outlook.com>
2023-09-22 12:47:30 -0300

    Update lesson
    Update lesson

---
+++ arrays.js
@@ -0,0 +1,8 @@
+//-------------------------------
+// Only change code below this line
+const myArray = ["This is a string", 2];
+//-------------------------------
+//Arrays dentro uma da outra
+// Only change code below this line
+const myArray2 = [[4, "oi"]];
+//-------------------------------
