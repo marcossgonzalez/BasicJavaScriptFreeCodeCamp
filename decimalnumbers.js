a8bd8fb368f430d74cc56ceecb80e0f3f1ff2d0a
Marcos Silveira Gonzalez Júnior <marcos.s.gonzalezjr@outlook.com>
2023-09-22 11:02:38 -0300

    Update lesson
    Update lesson

---
+++ decimalnumbers.js
@@ -0,0 +1,14 @@
+/*Também podemos armazenar números decimais em variáveis. Os números decimais às 
+vezes são chamados de números de ponto flutuante ou flutuantes.*/
+//Lição freecodecamp:
+const ourDecimal = 5.7;
+
+// Only change code below this line
+var myDecimal = ourDecimal;
+//------------------------------
+//Lição freecodecamp:
+const product = 2.0 * 2.5;
+//------------------------------
+//Lição freecodecamp:
+const quotient = 4.4 / 2.0; // Change this line
+//------------------------------
