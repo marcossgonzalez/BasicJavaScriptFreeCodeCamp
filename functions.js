d95a48d88ccfbc87545ee3171d5e64ecfe8342de
Marcos Silveira Gonzalez Júnior <marcos.s.gonzalezjr@outlook.com>
2023-09-23 14:53:54 -0300

    Update indentation
    update indentation

--- functions.js
+++ functions.js
@@ -1,63 +1,61 @@
 //----------------------------
-function reusableFunction(){
-    console.log("Hi World");
-  }
+function reusableFunction() {
+  console.log("Hi World");
+}
 reusableFunction();
 //----------------------------
-function functionWithArgs(param,param1){
-    console.log(param+param1);
-  }
-  functionWithArgs(7,9);
+function functionWithArgs(param, param1) {
+  console.log(param + param1);
+}
+functionWithArgs(7, 9);
 //----------------------------
-//Podemos passar valores para uma função com argumentos. 
+//Podemos passar valores para uma função com argumentos.
 //Você pode usar uma instrução return para enviar um valor de volta de uma função.
 //----------------------------
-function timesFive(arg){
-    return arg * 5;
+function timesFive(arg) {
+  return arg * 5;
 }
 //----------------------------
 //Variáveis ​​declaradas sem as palavras-chave let ou const são criadas automaticamente no escopo global.
 //----------------------------
 // Declare the myGlobal variable below this line
-
-
 function fun1() {
-    // Assign 5 to oopsGlobal here
-    oopsGlobal = 5;
+  // Assign 5 to oopsGlobal here
+  oopsGlobal = 5;
+}
+
+// Only change code above this line
+let myGlobal = 10;
+function fun2() {
+  let output = "";
+  if (typeof myGlobal != "undefined") {
+    output += "myGlobal: " + myGlobal;
   }
-  
-  // Only change code above this line
-  let myGlobal = 10;
-  function fun2() {
-    let output = "";
-    if (typeof myGlobal != "undefined") {
-      output += "myGlobal: " + myGlobal;
-    }
-    if (typeof oopsGlobal != "undefined") {
-      output += " oopsGlobal: " + oopsGlobal;
-    }
-    console.log(output);
+  if (typeof oopsGlobal != "undefined") {
+    output += " oopsGlobal: " + oopsGlobal;
   }
+  console.log(output);
+}
 //----------------------------
 //As variáveis ​​declaradas sem a palavra-chave tornam-se variáveis ​​globais, independentemente de onde sejam declaradas.
 //----------------------------
 function myLocalScope() {
-    // Only change code below this line
-    let myVar = 5;
-    console.log('inside myLocalScope', myVar);
-  }
-  myLocalScope();
-  
-  // Run and check the console
-  // myVar is not defined outside of myLocalScope
-  console.log('outside myLocalScope', myVar);
+  // Only change code below this line
+  let myVar = 5;
+  console.log("inside myLocalScope", myVar);
+}
+myLocalScope();
+
+// Run and check the console
+// myVar is not defined outside of myLocalScope
+console.log("outside myLocalScope", myVar);
 //----------------------------
 // Setup
 const outerWear = "T-Shirt";
 
 function myOutfit() {
   // Only change code below this line
-  let outerWear = "sweater"
+  let outerWear = "sweater";
   // Only change code above this line
   return outerWear;
 }
