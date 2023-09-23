6f42a43db65042dedf6adc51d5d2030fe39b3353
Marcos Silveira Gonzalez Júnior <marcos.s.gonzalezjr@outlook.com>
2023-09-23 14:57:50 -0300

    Update lesson
    Update lesson

--- functions.js
+++ functions.js
@@ -62,5 +62,30 @@ function myOutfit() {
 
 myOutfit();
 //----------------------------
+// Setup
+let sum = 0;
 
+function addThree() {
+  sum = sum + 3;
+}
+
+// Only change code below this line
+function addFive(){
+    sum += 5;
+}
+
+// Only change code above this line
+
+addThree();
+addFive();
+//----------------------------
+// Setup
+let processed = 0;
+
+function processArg(num) {
+  return (num + 3) / 5;
+}
+
+// Only change code below this line
+processed = processArg(7);
 //----------------------------
