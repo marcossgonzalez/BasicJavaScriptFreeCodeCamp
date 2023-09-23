724d2f8cba7bc01b12324f45cfbf2060eecc6527
Marcos Silveira Gonzalez Júnior <marcos.s.gonzalezjr@outlook.com>
2023-09-23 19:44:17 -0300

    update lesson
    update lesson

--- booleans.js
+++ booleans.js
@@ -91,3 +91,81 @@ function testGreaterThan(val) {
   
   testGreaterThan(10);
 //-------------------------------
+//O operador maior ou igual a (>=) compara os valores de dois números.
+//-------------------------------
+function testGreaterOrEqual(val) {
+  if (val>=20) {  // Change this line
+    return "20 or Over";
+  }
+
+  if (val>=10) {  // Change this line
+    return "10 or Over";
+  }
+
+  return "Less than 10";
+}
+
+testGreaterOrEqual(10);
+//-------------------------------
+//O operador menor que (<) compara os valores de dois números. 
+//Se o número à esquerda for menor que o número à direita, ele retornará verdadeiro.
+//-------------------------------
+function testLessThan(val) {
+  if (val<25) {  // Change this line
+    return "Under 25";
+  }
+
+  if (val<55) {  // Change this line
+    return "Under 55";
+  }
+
+  return "55 or Over";
+}
+
+testLessThan(10);
+//-------------------------------
+//O operador menor ou igual a (<=) compara os valores de dois números.
+//-------------------------------
+function testLessOrEqual(val) {
+  if (val<=12) {  // Change this line
+    return "Smaller Than or Equal to 12";
+  }
+
+  if (val<=24) {  // Change this line
+    return "Smaller Than or Equal to 24";
+  }
+
+  return "More Than 24";
+}
+
+testLessOrEqual(10);
+//-------------------------------
+//Às vezes, você precisará testar mais de uma coisa por vez. 
+//O operador lógico e (&&) retorna verdadeiro se e somente se os operandos à esquerda e à direita dele forem verdadeiros.
+//-------------------------------
+function testLogicalAnd(val) {
+  // Only change code below this line
+    if (val<=50 && val>=25) {
+      return "Yes";
+    }
+  // Only change code above this line
+  return "No";
+}
+
+testLogicalAnd(10);
+//-------------------------------
+//O operador lógico ou (||) retorna verdadeiro se algum dos operandos for verdadeiro. Caso contrário, retorna falso.
+//-------------------------------
+function testLogicalOr(val) {
+  // Only change code below this line
+
+  if (val<10 || val>20) {
+    return "Outside";
+  }
+
+  // Only change code above this line
+  return "Inside";
+}
+
+testLogicalOr(15);
+//-------------------------------
