fcd32f6fe5d8d9a4f78ed99452324addbfdfb8b0
Marcos Silveira Gonzalez Júnior <marcos.s.gonzalezjr@outlook.com>
2023-09-23 14:53:02 -0300

    Update lesson
    update lesson

--- functions.js
+++ functions.js
@@ -41,3 +41,28 @@ function fun1() {
 //----------------------------
 //As variáveis ​​declaradas sem a palavra-chave tornam-se variáveis ​​globais, independentemente de onde sejam declaradas.
 //----------------------------
+function myLocalScope() {
+    // Only change code below this line
+    let myVar = 5;
+    console.log('inside myLocalScope', myVar);
+  }
+  myLocalScope();
+  
+  // Run and check the console
+  // myVar is not defined outside of myLocalScope
+  console.log('outside myLocalScope', myVar);
+//----------------------------
+// Setup
+const outerWear = "T-Shirt";
+
+function myOutfit() {
+  // Only change code below this line
+  let outerWear = "sweater"
+  // Only change code above this line
+  return outerWear;
+}
+
+myOutfit();
+//----------------------------
+
+//----------------------------
