9de913530f255d1e4f4e2e6541d9388ca66f7dee
Marcos Silveira Gonzalez Júnior <marcos.s.gonzalezjr@outlook.com>
2023-09-23 14:48:00 -0300

    Update lesson
    Update lesson

---
+++ functions.js
@@ -0,0 +1,43 @@
+//----------------------------
+function reusableFunction(){
+    console.log("Hi World");
+  }
+reusableFunction();
+//----------------------------
+function functionWithArgs(param,param1){
+    console.log(param+param1);
+  }
+  functionWithArgs(7,9);
+//----------------------------
+//Podemos passar valores para uma função com argumentos. 
+//Você pode usar uma instrução return para enviar um valor de volta de uma função.
+//----------------------------
+function timesFive(arg){
+    return arg * 5;
+}
+//----------------------------
+//Variáveis ​​declaradas sem as palavras-chave let ou const são criadas automaticamente no escopo global.
+//----------------------------
+// Declare the myGlobal variable below this line
+
+
+function fun1() {
+    // Assign 5 to oopsGlobal here
+    oopsGlobal = 5;
+  }
+  
+  // Only change code above this line
+  let myGlobal = 10;
+  function fun2() {
+    let output = "";
+    if (typeof myGlobal != "undefined") {
+      output += "myGlobal: " + myGlobal;
+    }
+    if (typeof oopsGlobal != "undefined") {
+      output += " oopsGlobal: " + oopsGlobal;
+    }
+    console.log(output);
+  }
+//----------------------------
+//As variáveis ​​declaradas sem a palavra-chave tornam-se variáveis ​​globais, independentemente de onde sejam declaradas.
+//----------------------------
