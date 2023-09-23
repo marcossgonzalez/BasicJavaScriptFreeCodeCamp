8554db1a39996926d035578d16ea2bc25e2a1c67
Marcos Silveira Gonzalez Júnior <marcos.s.gonzalezjr@outlook.com>
2023-09-23 20:46:36 -0300

    update lesson
    update lesson

--- conditionals.js
+++ conditionals.js
@@ -1,69 +1,145 @@
 //------------------------------------------
 function testElse(val) {
-    let result = "";
-    // Only change code below this line
-  
-    if (val > 5) {
-      result = "Bigger than 5";
-    }
-  
-    else {
-      result = "5 or Smaller";
-    }
-  
-    // Only change code above this line
-    return result;
+  let result = "";
+  // Only change code below this line
+
+  if (val > 5) {
+    result = "Bigger than 5";
+  } else {
+    result = "5 or Smaller";
   }
-  
-  testElse(4);
+
+  // Only change code above this line
+  return result;
+}
+
+testElse(4);
 //------------------------------------------
 //Se você tiver várias condições que precisam ser abordadas, poderá encadear instruções if com instruções else if.
 //------------------------------------------
 function testElseIf(val) {
-    if (val > 10) {
-      return "Greater than 10";
-    } else if (val < 5) {
-      return "Smaller than 5";
-    } else {
+  if (val > 10) {
+    return "Greater than 10";
+  } else if (val < 5) {
+    return "Smaller than 5";
+  } else {
     return "Between 5 and 10";
   }
-  }
-  
-  testElseIf(7);
+}
+
+testElseIf(7);
 //------------------------------------------
 function orderMyLogic(val) {
-    if (val < 5) {
-      return "Less than 5";
-    } else if (val < 10) {
-      return "Less than 10";
-    } else {
-      return "Greater than or equal to 10";
-    }
+  if (val < 5) {
+    return "Less than 5";
+  } else if (val < 10) {
+    return "Less than 10";
+  } else {
+    return "Greater than or equal to 10";
   }
-  
-  orderMyLogic(7);
+}
+
+orderMyLogic(7);
 //------------------------------------------
 function testSize(num) {
-    // Only change code below this line
-    if(num < 5){
-        return "Tiny"
-    }
-    else if (num < 10){
-        return "Small"
-    }
-    else if (num < 15){
-        return "Medium"
-    }
-    else if (num < 20){
-        return "Large"
-    }
-    else if (num >= 20){
-        return "Huge"
-    }
-    
-    return "Change Me";
-    // Only change code above this line
+  // Only change code below this line
+  if (num < 5) {
+    return "Tiny";
+  } else if (num < 10) {
+    return "Small";
+  } else if (num < 15) {
+    return "Medium";
+  } else if (num < 20) {
+    return "Large";
+  } else if (num >= 20) {
+    return "Huge";
   }
-  
-  testSize(7);
+
+  return "Change Me";
+  // Only change code above this line
+}
+
+testSize(7);
+//------------------------------------------
+const names = [
+  "Hole-in-one!",
+  "Eagle",
+  "Birdie",
+  "Par",
+  "Bogey",
+  "Double Bogey",
+  "Go Home!",
+];
+
+function golfScore(par, strokes) {
+  // Only change code below this line
+  if (strokes == 1) {
+    return names[0];
+  } else if (strokes == par - 1) {
+    return names[2];
+  } else if (strokes <= par - 2) {
+    return names[1];
+  } else if (strokes == par + 1) {
+    return names[4];
+  } else if (strokes == par + 2) {
+    return names[5];
+  } else if (strokes == par) {
+    return names[3];
+  }
+  return names[names.length - 1];
+  // Only change code above this line
+}
+
+golfScore(5, 4);
+//------------------------------------------
+//Se você precisar combinar um valor com muitas opções, poderá usar uma instrução switch.
+//Uma instrução switch compara o valor com as instruções case que definem vários valores possíveis.
+//------------------------------------------
+function caseInSwitch(val) {
+  let answer = "";
+  // Only change code below this line
+  switch (val) {
+    case 1:
+      answer = "alpha";
+      break;
+    case 2:
+      answer = "beta";
+      break;
+    case 3:
+      answer = "gamma";
+      break;
+    case 4:
+      answer = "delta";
+  }
+  // Only change code above this line
+  return answer;
+}
+
+caseInSwitch(1);
+//------------------------------------------
+//Em uma instrução switch talvez você não consiga especificar todos os valores possíveis como instruções case.
+//Em vez disso, você pode adicionar a instrução padrão que será executada se nenhuma instrução case correspondente for encontrada.
+//------------------------------------------
+function switchOfStuff(val) {
+  let answer = "";
+  // Only change code below this line
+  switch (val) {
+    case "a":
+      return "apple";
+      break;
+    case "b":
+      return "bird";
+      break;
+    case "c":
+      return "cat";
+      break;
+    default:
+      return "stuff";
+  }
+
+  // Only change code above this line
+  return answer;
+}
+
+switchOfStuff(1);
 //------------------------------------------
