2ada28d505a2b1b87b3975935975f732a9808e61
Marcos Silveira Gonzalez Júnior <marcos.s.gonzalezjr@outlook.com>
2023-09-23 21:17:05 -0300

    update lesson
    update lesson

--- conditionals.js
+++ conditionals.js
@@ -169,5 +169,42 @@ function sequentialSizes(val) {
     return answer;
   }
   
-  sequentialSizes(2);
+  sequentialSizes(1);
+//------------------------------------------
+function chainToSwitch(val) {
+    let answer = "";
+    // Only change code below this line
+    switch(val){
+    case "bob":
+      answer = "Marley";
+      break;
+      case 42: 
+      answer = "The Answer";
+      break;
+      case 1:
+      answer = "There is no #1";
+      break;
+      case 99:
+      answer = "Missed me by this much!";
+      break;
+      case 7:
+      answer = "Ate Nine";
+      break;
+    }
+  
+    // Only change code above this line
+    return answer;
+  }
+  
+  chainToSwitch(7);
+//------------------------------------------
+function isLess(a, b) {
+    // Only change code below this line
+     return (a < b)
+    
+    
+    // Only change code above this line
+  }
+  
+  isLess(10, 15);
 //------------------------------------------
