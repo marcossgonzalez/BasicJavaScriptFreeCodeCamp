8b37609e7eb29250863bad2350ec17ec3e28ec7a
Marcos Silveira Gonzalez Júnior <marcos.s.gonzalezjr@outlook.com>
2023-09-24 13:20:27 -0300

    update lesson
    update lesson

---
+++ functionpatterns.js
@@ -0,0 +1,47 @@
+//-------------------------------------------------
+// Setup
+function abTest(a, b) {
+    // Only change code below this line
+    if (a<0 || b<0){
+      return undefined;
+    }
+  
+  
+    // Only change code above this line
+  
+    return Math.round(Math.pow(Math.sqrt(a) + Math.sqrt(b), 2));
+  }
+  
+abTest(2,2);
+//-------------------------------------------------
+let count = 0;
+
+function cc(card) {
+  // Only change code below this line
+  switch(card){
+    case 2:
+    case 3:
+    case 4:
+    case 5:
+    case 6:
+    count += 1;
+    break;
+    case 10:
+    case 'J':
+    case 'Q':
+    case 'K':
+    case 'A':
+    count -= 1;
+  
+  }
+  if (count >= 1){
+    let bet = " Bet";
+    return (count + bet);
+  }
+  let hold = " Hold"
+    return (count + hold);
+  // Only change code above this line
+}
+
+cc(2); cc(3); cc(7); cc('K'); cc('A');
+//-------------------------------------------------
