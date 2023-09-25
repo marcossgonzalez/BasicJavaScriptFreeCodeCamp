d249a017db6036e5a4297ce1c30e177f0fee9917
Marcos Silveira Gonzalez Júnior <marcos.s.gonzalezjr@outlook.com>
2023-09-25 13:55:35 -0300

    update lesson
    update lesson

--- loops.js
+++ loops.js
@@ -98,3 +98,13 @@ do {
   i++;
 } while( i<11);
 //----------------------------------
+function sum(arr, n) {
+    // Only change code below this line
+    if (n <= 0 ){
+      return 0;
+    } else{
+      return sum(arr, n-1) + arr[n-1]
+    }
+    // Only change code above this line
+  }
+//-----------------------------------
